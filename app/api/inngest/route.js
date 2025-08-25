6408a7f42cd361524abfbcf853c1c510f3ca5793
AnujPratap-96 <offcialthakur94@gmail.com>
2025-08-25 22:30:44 +0530

    Update environment variables and modify API route methods

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -3,7 +3,7 @@ import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
 
 
-export const {GET , POST, DELETE} =  serve({
+export const {GET , POST, PUT} =  serve({
   client: inngest,
   functions: [
    syncUserCreation,
