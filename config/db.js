dd1aab3ead26f21fd6546eee9c7cbd6180947841
AnujPratap-96 <offcialthakur94@gmail.com>
2025-08-27 13:27:42 +0530

    Remove database name from MongoDB connection URI

--- config/db.js
+++ config/db.js
@@ -19,7 +19,7 @@ async function connectDB() {
     };
 
     cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/QuickCart`, opts)
+      .connect(`${process.env.MONGODB_URI}`, opts)
       .then((mongoose) => {
         return mongoose;
       });
